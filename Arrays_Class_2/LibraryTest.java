9ae04a149347ded61f9c2ef9845e0c17ac036ff3
Carlos Andres Castrillon Oviedo <carlos.castrillonov@campusucc.edu.co>
2025-08-17 13:18:25 -0500

    implementacion de instancia de Scanner

--- Arrays_Class_2/LibraryTest.java
+++ Arrays_Class_2/LibraryTest.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class LibraryTest {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+
         //creando la libreria
         Library library = new Library(5);
 
@@ -9,5 +14,13 @@ public class LibraryTest {
         library.addBook(new Book("Cien años de soledad", "Gabriel García Márquez", "789", false));
         library.addBook(new Book("El Principito", "Antoine de Saint-Exupéry", "101", true));
         library.addBook(new Book("La sombra del viento", "Carlos Ruiz Zafón", "202", false));
+
+        // mostrando cada libro
+        library.displayAllBooks();
+
+        // buscar libro por titulo
+        System.out.println("Ingresa el nombre del libro");
+        Book busqueda : library.busquedaLibros()
+
     }
 }
