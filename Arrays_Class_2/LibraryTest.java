980a0511330073baf394959ab34d679ed75b8b9d
Carlos Andres Castrillon Oviedo <carlos.castrillonov@campusucc.edu.co>
2025-08-17 13:11:42 -0500

    creacion de libros

--- Arrays_Class_2/LibraryTest.java
+++ Arrays_Class_2/LibraryTest.java
@@ -2,5 +2,12 @@ public class LibraryTest {
     public static void main(String[] args){
         //creando la libreria
         Library library = new Library(5);
+
+        // alimentando el inventario
+        library.addBook(new Book("1984", "Orwell", "123", true));
+        library.addBook(new Book("Don Quijote de la Mancha", "Cervantes", "456", true));
+        library.addBook(new Book("Cien años de soledad", "Gabriel García Márquez", "789", false));
+        library.addBook(new Book("El Principito", "Antoine de Saint-Exupéry", "101", true));
+        library.addBook(new Book("La sombra del viento", "Carlos Ruiz Zafón", "202", false));
     }
 }
